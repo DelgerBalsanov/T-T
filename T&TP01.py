c96eca1cac355ad57d488a027be617213ce551bc
DelgerBalsanov <134125808+DelgerBalsanov@users.noreply.github.com>
2023-12-27 14:34:26 +0700

    Add files via upload

---
+++ T&TP01.py
@@ -0,0 +1,36 @@
+def input_hello():
+    return 'Hello!'
+
+
+def input_phone():
+    return 'Give me the phone'
+
+
+def input_meneger1():
+    return 'What kind of phone do you need?'
+
+
+def input_buyer1():
+    return 'iPhone 14 Pro Max'
+
+
+def input_meneger2():
+    return 'There he is! Heres your iPhone 14 Pro Max!'
+
+
+def input_buyer2():
+    return 'Thank you'
+
+
+def main():
+    print('-', input_hello())
+    print('-', input_hello())
+    print('-', input_phone())
+    print('-', input_meneger1())
+    print('-', input_buyer1())
+    print('-', input_meneger2())
+    print('-', input_buyer2())
+
+
+if __name__ in '__main__':
+    main()
